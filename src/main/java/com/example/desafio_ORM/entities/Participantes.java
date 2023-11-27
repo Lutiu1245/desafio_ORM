14c6724e55bfd46d88ea5f5e3064af2895340089
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-11-27 10:28:10 -0300

    Initial commit

---
+++ src/main/java/com/example/desafio_ORM/entities/Participantes.java
@@ -0,0 +1,59 @@
+package com.example.desafio_ORM.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_participantes")
+public class Participantes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+
+    public Participantes(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Participantes that = (Participantes) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/example/desafio_ORM/DesafioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_ORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
